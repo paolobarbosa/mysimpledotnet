4ae9c5e36fbb9bde47a13bdb146288932420004c
Paolo Barbosa <iamseijvage@gmail.com>
2023-10-05 12:05:01 +0800

    initial commit

---
+++ Models/Employee.cs
@@ -0,0 +1,12 @@
+﻿namespace MVCwithWebAPI.Models
+{
+    public class Employee
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public string Gender { get; set; }
+        public string Company { get; set; }
+        public string Designation { get; set; }
+    }
+}
